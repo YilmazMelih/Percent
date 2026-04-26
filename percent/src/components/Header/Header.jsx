225779c0198af5955523b9bf6fe241111598f69b
yueue-ctrl <fallonyueue@gmail.com>
2026-04-26 14:25:56 -0400

    Changed the logo

--- percent/src/components/Header/Header.jsx
+++ percent/src/components/Header/Header.jsx
@@ -5,6 +5,8 @@ import { useModal } from "../../contexts/ModalContext";
 import arrowLeft from "../../assets/images/arrow-left.svg";
 import arrowRight from "../../assets/images/arrow-right.svg";
 import icon from "../../assets/images/icon.png";
+import percentGif from "../../assets/images/Percent.gif";
+import percentStatic from "../../assets/images/Percent.png"; // Import the static PNG
 import downloadIcon from "../../assets/images/download.svg";
 import ExportPage from "../Pages/Export/Export";
 
@@ -17,6 +19,21 @@ function Header() {
     const exportPageRef = useRef(null);
     const navContainerRef = useRef(null);
     const exportOpenedAtRef = useRef(0);
+    const [logoSrc, setLogoSrc] = useState(percentStatic); // Default to the static PNG
+
+    // Preload the GIF to prevent flickering on first hover
+    useEffect(() => {
+        const img = new Image();
+        img.src = percentGif;
+    }, []);
+
+    const handleLogoEnter = () => {
+        setLogoSrc(percentGif);
+    };
+
+    const handleLogoLeave = () => {
+        setLogoSrc(percentStatic);
+    };
 
     useEffect(() => {
         // This logic handles the animation when navigating from the home page.
@@ -48,9 +65,14 @@ function Header() {
     return (
         <header className="header">
             <div className="header-brand">
-                <Link to="/" className="header-title">
-                    <img src={icon} alt="Logo" className="header-logo" />
-                </Link>
+                <Link
+                     to="/"
+                     className="header-title"
+                     onMouseEnter={() => setLogoSrc(percentGif)}
+                     onMouseLeave={() => setLogoSrc(percentStatic)}
+                 >
+                     <img src={logoSrc} alt="Logo" className="header-logo" />
+                 </Link>
             </div>
             <nav className="header-nav">
                 {isHomePage ? (
